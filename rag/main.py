ec99d07a7c556b815d83210322e3f71e0d8c8e98
rafael guedes <rafael.guedes@marleyspoon.com>
2023-11-21 12:33:24 +0000

    update main

--- rag/main.py
+++ rag/main.py
@@ -1,11 +1,14 @@
 from encoder.encoder import Encoder
 from generator.generator import Generator
 from langchain.document_loaders import HuggingFaceDatasetLoader
+from langchain.docstore.document import Document
 from retriever.vector_db import VectorDatabase
 
 # load dataset
-loader = HuggingFaceDatasetLoader("luisroque/instruct-python-500k", 'answer')
-docs = loader.load()[:1000]
+loader = HuggingFaceDatasetLoader("luisroque/instruct-python-llama2-20k", 'text')
+docs = loader.load()
+train = [Document(page_content=x.page_content.split('[/INST]')[1]) for x in docs[:-1000]]
+test = [Document(page_content=x.page_content.split('<</SYS>>')[2]) for x in docs[-1000:]]
 
 TEMPLATE = """
 Use the following pieces of context to answer the question at the end. 
@@ -18,17 +21,14 @@ encoder = Encoder()
 faiss_db = VectorDatabase()
 generator = Generator(TEMPLATE)
 
-passages = faiss_db.create_passages_from_documents(docs)
+passages = faiss_db.create_passages_from_documents(train)
 faiss_db.store_passages_db(passages, encoder.encoder)
 
-from datasets import load_dataset
-data = load_dataset("luisroque/instruct-python-500k")
-query = data['train'][3]['question']
-
+query = test[0].page_content
 context = faiss_db.retrieve_most_similar_document(query)
 generator.define_prompt()
 print(generator.get_answer(context, query))
 
 print(query)
-print(data['train'][3]['answer'])
 print(context)
+print(docs[-1000].page_content.split('[/INST]')[1])
