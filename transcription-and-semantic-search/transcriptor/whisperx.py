127f2ea49126ce9d8b186cc10f4b9a62f7619fcf
rafael guedes <rafael.guedes@marleyspoon.com>
2023-12-04 19:26:44 +0000

    add latest results

--- transcription-and-semantic-search/transcriptor/whisperx.py
+++ transcription-and-semantic-search/transcriptor/whisperx.py
@@ -1,5 +1,5 @@
 import whisper
-from whisperx import align, load_align_model
+import whisperx
 
 from base.config import Config
 
@@ -7,11 +7,25 @@ from base.config import Config
 class WhisperX(Config):
     """Transcription Model"""
 
-    def __init__(self):
+    def __init__(self, model_name: str):
+        """
+        Init method
+        Args:
+            model (str): whisper or whisper x
+        """
         super().__init__()
-        self.model = whisper.load_model(
-            self.config["transcriptor"]["model"], self.config["transcriptor"]["device"]
-        )
+        self.model_name = model_name
+        if self.model_name == "whisper":
+            self.model = whisper.load_model(
+                self.config["transcriptor"][model_name]["model"],
+                self.config["transcriptor"]["device"],
+            )
+        else:
+            self.model = whisperx.load_model(
+                self.config["transcriptor"][model_name]["model"],
+                self.config["transcriptor"]["device"],
+                compute_type=self.config["transcriptor"][model_name]["compute_type"],
+            )
 
     def transcribe(self, audio_path: str) -> dict:
         """
@@ -21,13 +35,15 @@ class WhisperX(Config):
         Returns:
             result_aligned (dict): dictionary with segments and metadata
         """
+        if self.model_name == "whisperx":
+            audio_path = whisperx.load_audio(audio_path)
 
         result = self.model.transcribe(audio_path)
-        model_a, metadata = load_align_model(
+        model_a, metadata = whisperx.load_align_model(
             language_code=result["language"],
             device=self.config["transcriptor"]["device"],
         )
-        result_aligned = align(
+        result_aligned = whisperx.align(
             result["segments"],
             model_a,
             metadata,
