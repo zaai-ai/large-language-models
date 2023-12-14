0f9bdf01380fe3041b569944b8559041cb9b5762
rafael guedes <rafael.guedes@marleyspoon.com>
2023-12-14 18:43:17 +0000

    1st commit

---
+++ translation/classifier/language_classifier.py
@@ -0,0 +1,24 @@
+from base.config import Config
+from transformers import pipeline
+import yaml
+
+
+class LanguageDetector(Config):
+    """XLMRoBERTa for language detection"""
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.model = pipeline("text-classification", model=self.config["classifier"]["model"], device=self.config["classifier"]["device"])
+
+    def detect_language(self, document: str) -> str:
+        """
+        Detects the language of a document
+        Args:
+            document (str): document for language detection
+        Returns:
+            str: language
+        """
+
+        lang = self.model([document])[0]['label']
+        
+        return self.lang_map[lang]
